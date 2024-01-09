596ed35101da31bef629e349dd2fcb57551ca43d
Carol Silva <contatocarolinebs@gmail.com>
2024-01-09 14:47:05 -0300

    add component Content

--- src/Components/Content/index.tsx
+++ src/Components/Content/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import Coin from '../Coin';
+import './index.css'
 
 const Content = () => {
-  
+  //como fazer o fetch e ver o que a api ta retornando
+// Import 'node-fetch' if you are using this in a Node.js environment
+
+const [cryptoData, setCryptoData] = useState<any[]>([]);
+
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      try {
+        const response = await fetch('http://localhost:3000');
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const {data} = await response.json();
+        setCryptoData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);    
+    
+console.log(cryptoData, 'crypto');
+
   return (
-    <div>Content</div>
+    <div className='content-box'>{
+      cryptoData.map((element) => <Coin name={element.name} price={element.name} imgSrc={element.symbol} key={element.id}/>)
+  }
+  </div>
   )
 }
 
