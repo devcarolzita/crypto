49b3472afd13aa313f80f0c1e0289e413389666d
Carol Silva <contatocarolinebs@gmail.com>
2024-01-09 10:07:29 -0300

    add news archives

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>Header</p>
+     <p>Aqui é a home</p>
+      <div>
+        <div>
+          <ul>
+            <li> <i></i> <p>nome</p> <p>2,99</p></li>
+          </ul>
+        </div>
+      </div>
+      <p>footer</p>
+    </>
+  )
+}
+
+export default App
